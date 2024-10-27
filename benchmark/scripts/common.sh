0378e1f3d623f023621a68a2c960ea71968d062d
Raiki Tamura <tamaron1203@gmail.com>
2024-10-27 17:18:27 +0900

    bench: wip

--- benchmark/scripts/common.sh
+++ benchmark/scripts/common.sh
@@ -14,3 +14,18 @@ get_half_elapsed_time() {
     local half_time=$(echo "$elapsed_time / 2" | bc -l)    
     echo "$half_time"
 }
+
+get_elapsed_time() {
+    local start_time=$(date +%s.%N)
+    "$@" > /dev/null 2>&1
+    local code=$?
+    local end_time=$(date +%s.%N)
+    if [ $code -ne 0 ]; then
+        echo "Error: exit with code $code"
+        exit 1
+    fi
+
+    local elapsed_time=$(echo "$end_time - $start_time" | bc)
+    echo "$elapsed_time"
+}
+
---
+++ benchmark/scripts/snapshot-size-criu.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+SCRIPT_DIR=$(dirname $(realpath $0))
+source $SCRIPT_DIR/common.sh
+
+NUM_RUNS=3
+CHECKPOINT_DIR="checkpoint"
+SKIP_BUILD=1
+
+LABBENCH_DIR=${SCRIPT_DIR}/../computer-lab-benchmark
+LLAMA2_DIR=${SCRIPT_DIR}/../llama2-c
+BENCH_DIR=${SCRIPT_DIR}/..
+
+measure_criu_checkpoint_size() {
+    local exe_name=$(echo $1 | sed 's/[^a-zA-Z0-9_-]//g')
+    echo "--- $exe_name ---"
+    echo "command: $@"
+    # grep with first five characters of the command
+    local exe_name=$(echo $exe_name | cut -c1-5)
+
+
+    # check command run without error
+    "$@" > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "Error: failed to run command"
+        exit 1
+    fi
+
+    local half_elapsed_time=$(get_half_elapsed_time "$@")
+    echo "half elapsed time: $half_elapsed_time"
+
+    file_sizes=()
+    for i in $(seq 1 $NUM_RUNS); do
+        rm -rf -f $CHECKPOINT_DIR
+        if [ $? -ne 0 ]; then
+            echo "Error: failed to remove $CHECKPOINT_DIR"
+            exit 1
+        fi
+        mkdir $CHECKPOINT_DIR
+        sleep 0.3
+
+        "$@" > /dev/null 2>&1 & \
+            sleep $half_elapsed_time & \
+            criu dump --shell-job -t $(pgrep $exe_name) -D checkpoint
+        sleep 0.3
+        
+        local file_size=$(du -sb "$CHECKPOINT_DIR" | cut -f1)
+        echo "$i: File size: $file_size"
+        file_sizes+=($file_size)
+    done
+
+    # avg and mean
+    local total_file_size=0
+    for file_size in ${file_sizes[@]}; do
+        total_file_size=$(echo "$total_file_size + $file_size" | bc)
+    done
+    local average_file_size=$(echo "$total_file_size / $NUM_RUNS" | bc)
+    echo "Average: $average_file_size"
+    # mean
+    local sorted_file_sizes=($(echo ${file_sizes[@]} | tr ' ' '\n' | sort -n))
+    local num_file_sizes=${#sorted_file_sizes[@]}
+    local median_file_size=${sorted_file_sizes[$((num_file_sizes / 2))]}
+    echo "Mean: $median_file_size"
+}
+
+if [ $SKIP_BUILD -eq 0 ]; then
+    echo "Compiling c files with clang"
+    cd $BENCH_DIR
+    clang -O1 ${LABBENCH_DIR}/nbody.c -o "nbody-native" -O1 -Wl,-lm
+    clang -O1 ${LABBENCH_DIR}/binary-trees.c -o "binary-trees-native" -O1
+fi
+
+cd $LLAMA2_DIR
+#measure_criu_checkpoint_size "./llama2-c-x86-64" "model.bin" "-n" 0 "-i" 'Once upon a time'
+cd $BENCH_DIR
+#measure_criu_checkpoint_size "./nbody-native" 10000000
+measure_criu_checkpoint_size "./binary-trees-native" 18
+
