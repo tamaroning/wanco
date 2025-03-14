ea5fbefcb21306a8f07c27cde6223200e1251f97
Raiki Tamura <tamaron1203@gmail.com>
2025-03-15 00:11:00 +0900

    add -O3 option

--- benchmark/scripts/build.sh
+++ benchmark/scripts/build.sh
@@ -4,22 +4,22 @@ source $SCRIPT_DIR/common.sh
 
 cd $BENCH_DIR
 echo "Compiling llama2"
-wanco ${LLAMA2_DIR}/llama2-c.wasm -o "llama2"
+wanco -O3 ${LLAMA2_DIR}/llama2-c.wasm -o "llama2"
 echo "Compiling llama2 with cr"
-wanco --enable-cr ${LLAMA2_DIR}/llama2-c.wasm -o "llama2-c-cr"
-wanco --enable-cr --disable-loop-cr ${LLAMA2_DIR}/llama2-c.wasm -o "llama2-c-no-loop"
+wanco -O3 --enable-cr ${LLAMA2_DIR}/llama2-c.wasm -o "llama2-c-cr"
+wanco -O3 --enable-cr --disable-loop-cr ${LLAMA2_DIR}/llama2-c.wasm -o "llama2-c-no-loop"
 
 echo "Compiling nbody"
 wanco ${LABBENCH_DIR}/nbody.c.wasm -o "nbody"
 echo "Compiling nbody with cr"
-wanco --enable-cr ${LABBENCH_DIR}/nbody.c.wasm -o "nbody-cr"
-wanco --enable-cr --disable-loop-cr ${LABBENCH_DIR}/nbody.c.wasm -o "nbody-no-loop"
+wanco -O3 --enable-cr ${LABBENCH_DIR}/nbody.c.wasm -o "nbody-cr"
+wanco -O3 --enable-cr --disable-loop-cr ${LABBENCH_DIR}/nbody.c.wasm -o "nbody-no-loop"
 
 echo "Compiling binary-trees"
 wanco ${LABBENCH_DIR}/binary-trees.c.wasm -o "binary-trees"
 echo "Compiling binary-trees with cr"
-wanco --enable-cr ${LABBENCH_DIR}/binary-trees.c.wasm -o "binary-trees-cr"
-wanco --enable-cr --disable-loop-cr ${LABBENCH_DIR}/binary-trees.c.wasm -o "binary-trees-no-loop"
+wanco -O3 --enable-cr ${LABBENCH_DIR}/binary-trees.c.wasm -o "binary-trees-cr"
+wanco -O3 --enable-cr --disable-loop-cr ${LABBENCH_DIR}/binary-trees.c.wasm -o "binary-trees-no-loop"
 
 #echo "Compiling sqlite"
 #wanco --enable-cr ${SQLITE_DIR}/sqlite_example.wasm -o "sqlite_example-cr"
